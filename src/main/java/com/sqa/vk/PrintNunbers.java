f62436871c3f58138256f622b26a2efc9250e7a9
Vladimir Kononov <vladimirkon@gmail.com>
2017-08-23 10:19:13 -0700

    Create PrintNumbers.java

---
+++ src/main/java/com/sqa/vk/PrintNunbers.java
@@ -0,0 +1,17 @@
+package com.sqa.vk;
+
+public class PrintNunbers {
+
+	public static void main(String[] args) {
+		int num = 10;
+		if (num < 10) {
+			System.out.println("number is smaller than 10");
+		} else {
+			if (num > 10) {
+				System.out.println("number is greater thatn 10");
+			} else {
+				System.out.println("number equals 10");
+			}
+		}
+	}
+}
